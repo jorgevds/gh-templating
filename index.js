0402548bf73ef6a59cacb5aec9fe7520291e414d
jorgevds <jorgevandesompel@gmail.com>
2023-02-01 09:42:33 +0100

    Expand search options to include possible templates directory. Fix gh cli command.

--- index.js
+++ index.js
@@ -4,52 +4,91 @@ import { execSync } from "child_process";
 import path from "path";
 
 const main = (pathToTemplates) => {
-  let choices = [];
+    let config = { path: pathToTemplates, choices: [] };
 
-  try {
-    choices = fs.readdirSync(pathToTemplates);
-  } catch (error) {
-    console.error(
-      `Fatal error: no path provided or found to templates directory. Received ${pathToTemplates}`
-    );
-  }
+    if (pathToTemplates) {
+        try {
+            const templates = fs
+                .readdirSync(pathToTemplates)
+                .filter((file) => file.includes(".md"));
 
-  if (choices.length > 0) {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "PR Template",
-          message: "Which pull request template would you like to use?",
-          choices,
-        },
-      ])
-      .then((answers) => {
-        const template = answers["PR Template"];
-        const pathToTemplate = path.join(
-          process.cwd(),
-          ...`${pathToTemplates}/${template}`.split("/")
-        );
-
-        console.log | "--------------------------------------";
-        console.log(
-          execSync(
-            `gh pr create --title "My new pr" --body-file ${pathToTemplate}`
-          )
-        );
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else went wrong
+            choices = templates;
+        } catch (error) {
+            console.error(
+                `Fatal error: no path provided or found to templates directory. Received ${pathToTemplates}`
+            );
         }
-      });
-  } else {
-    console.error(
-      `Fatal error: no choices available. Expected any choice, but found none.`
-    );
-  }
+    } else if (
+        fs.existsSync(
+            path.join(process.cwd(), ...`.github/templates`.split("/"))
+        )
+    ) {
+        {
+            try {
+                config.path = path.join(...`.github/templates`.split("/"));
+                const githubTemplates = fs
+                    .readdirSync(
+                        path.join(
+                            process.cwd(),
+                            ...`.github/templates`.split("/")
+                        )
+                    )
+                    .filter((file) => file.includes(".md"));
+
+                config.choices = githubTemplates;
+            } catch (error) {
+                console.error(
+                    `No templates found in: ROOT:.github/templates. Looking in ROOT:.github/`
+                );
+            }
+        }
+    } else {
+        try {
+            config.path = path.join(...`.github/`.split("/"));
+
+            const githubTemplates = fs
+                .readdirSync(path.join(process.cwd(), ...`.github/`.split("/")))
+                .filter((file) => file.includes(".md"));
+
+            config.choices = githubTemplates;
+        } catch (error) {
+            console.error(
+                `Fatal error: no templates found in codebase. Please specify a path to your templates.`
+            );
+        }
+    }
+
+    if (config.choices.length > 0) {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "PR Template",
+                    message:
+                        "Which pull request template would you like to use?",
+                    choices: config.choices,
+                },
+            ])
+            .then((answers) => {
+                const template = answers["PR Template"];
+                const pathToTemplate = path.join(
+                    ...`${config.path}/${template}`.split("/")
+                );
+
+                execSync(
+                    `gh pr create --title "My new pr" --body-file ${pathToTemplate}`
+                );
+            })
+            .catch((error) => {
+                console.error(
+                    `Inquirer error: something went wrong processing your answer: ${error}`
+                );
+            });
+    } else {
+        console.error(
+            `Fatal error: no choices available. Expected any choice, but found none.`
+        );
+    }
 };
 
 main(process.argv[2]);
