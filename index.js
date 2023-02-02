020db9ff51140f5bd84d73fb627f4eac17b9e5a7
jorgevds <jorgevandesompel@gmail.com>
2023-02-02 08:37:52 +0100

    Fix use command args overtaking one another. Fix use command supplying path should lead to results. Update readme to reflect new status of title arg as option. Bump patch version.

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const program = new Command();
 program
     .command("use")
     .description("Select a template and create a new PR")
-    .argument("[path]", "Path to templates directory - string - optional")
-    .argument("[title]", "Title of your new PR - string - optional")
-    .action((path, title) => use(path, title));
+    .argument("[path]", "Path to templates directory - string - optional", "")
+    .option(
+        "-t, --title <string>",
+        "Title of your new PR - string - optional",
+        ""
+    )
+    .action((path, { title }) => use(path, title));
 
 program.parse(process.argv);
